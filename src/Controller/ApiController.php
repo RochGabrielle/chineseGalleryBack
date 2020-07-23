c460f61090d771b2b1893939799560c5036116bb
rochGabrielle <roch.gab@gmail.com>
2020-07-23 18:57:57 +0200

    Backoffice api

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -17,6 +17,7 @@ use App\Entity\Article;
 
 class ApiController extends Controller
 {
+   protected $languages = array("en_gb", "fr_fr");
 
     /**
      * @Route("/api/translation_add", name="add_translation", methods={"POST"})
@@ -25,28 +26,35 @@ class ApiController extends Controller
     {
         $content = json_decode($request->getContent(), true);
 
-        if (isset($content["placeholder"]) && isset($content["translation"]) && isset($content["entity"]) && isset($content["lang"])) {
+        if (isset($content["placeholder"]) && isset($content["fr_fr"]) && isset($content["en_gb"]) && isset($content["entity"])) {
             $entityClass = 'App\Entity\\'.ucfirst($content["entity"]);
             if (class_exists($entityClass)) {
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entity = $entityManager->getRepository($entityClass)->findOneByPlaceholder($content["placeholder"]);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entity = $entityManager->getRepository($entityClass)->findOneByPlaceholder($content["placeholder"]);
 
-            if ($entity) {
-                $entity->translate($content["lang"])->setName($content["translation"]);
-                $entity->mergeNewTranslations();
-                $content = $content["lang"];              
-            } else {
-                $entity = new $entityClass();
-                $entity->setPlaceholder($content["placeholder"]);
-                $entity->translate($content["lang"])->setName($content["translation"]);
-                $entity->mergeNewTranslations();
-                $content = "new ".$content["entity"]." created.";
-            }       
-            
-            $entityManager->persist($entity);
-            $entityManager->flush();
-        
+                if ($entity) {
+                    foreach($this->languages as $lang) {
+                        var_dump($content[strval($lang)]);
+                        if(empty($entity->translate($lang)->getName()) || ($entity->translate($lang)->getName() != $content[$lang]) ) {
+                            $entity->translate($lang)->setName($content[$lang]);
+                            $entity->mergeNewTranslations();
+                            $content = "update of ". $content["placeholder"] . " in " .$content["entity"];   
+                        }
+                    }
+
+                } else {
+                    $entity = new $entityClass();
+                    $entity->setPlaceholder($content["placeholder"]);
+                    foreach($this->languages as $lang) {
+                        $entity->translate($lang)->setName($content[$lang]);
+                        $entity->mergeNewTranslations();
+                        $content = "new ".$content["entity"]." created.";
+                    }       
+                }
+                $entityManager->persist($entity);
+                $entityManager->flush();
+
             } else {
                 $content = "the ".$entity." entity doesn't exist.";
             }
@@ -68,22 +76,22 @@ class ApiController extends Controller
         if (class_exists($entityClass)) {
             $entityManager = $this->getDoctrine()->getManager();
 
-        
-        $placeholders = $entityManager->getRepository($entityClass)->findAll();
-        $translations = array();
-        if( !empty($placeholders)) {
-            foreach($placeholders as $placeholder){
-                $translations[$placeholder->getPlaceholder()] = $placeholder->translate($lang)->getName();
-            }
+
+            $placeholders = $entityManager->getRepository($entityClass)->findAll();
+            $translations = array();
+            if( !empty($placeholders)) {
+                foreach($placeholders as $placeholder){
+                    $translations[$placeholder->getPlaceholder()] = $placeholder->translate($lang)->getName();
+                }
 
            //$data = $this->get('jms_serializer')->serialize($translations, 'json');
-           $data = json_encode($translations) ;
-       } else {
-        $data = $this->get('jms_serializer')->serialize($placeholder, 'json'); 
-       }
-   } else {
-    $data = "this class doesn't exist";
-   }
+                $data = json_encode($translations) ;
+            } else {
+                $data = $this->get('jms_serializer')->serialize($placeholder, 'json'); 
+            }
+        } else {
+            $data = "this class doesn't exist";
+        }
 
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
@@ -94,15 +102,15 @@ class ApiController extends Controller
       /**
      * @Route("/api/article_add", name="add_article", methods={"POST"})
      */
-    public function addArticleAction( Request $request)
-    {
+      public function addArticleAction( Request $request)
+      {
         $content = json_decode($request->getContent(), true);
-      
-      if($content) {
+
+        if($content) {
         //
         //$navigationMenu->translate('en')->setMenuName();
 
-        
+
         //$entityManager->flush();   
         } else {
             $content = "le json est invalide";
@@ -121,50 +129,52 @@ class ApiController extends Controller
     {
         $content = json_decode($request->getContent(), true);
 
-        if (isset($content["name"]) && isset($content["birth"]) && isset($content["death"]) && isset($content["description"]), isset($content["lang"]), isset($content["entity"])) {
+        if (isset($content["name"]) && 
+            isset($content["birth"]) && 
+            isset($content["death"]) && 
+            isset($content["description"]) &&
+            isset($content["lang"]) &&
+            isset($content["entity"])) {
             $entityClass = 'App\Entity\\'.ucfirst($content["entity"]);
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entity = $entityManager->getRepository($entityClass)->findOneByName($content["name"]);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entity = $entityManager->getRepository($entityClass)->findOneByName($content["name"]);
 
-            if ($entity) {
-                if($entity->getBirth() != $content["birth"]) {
-                    $entity->setBirth($content["birth"]);
-                }
-                if($entity->getDeath($content["death"])!=  $content["death"] ) {
-                    $entity->setDeath($content["death"]);
-                }
-                if((!empty($entity->translate($content["lang"])->getDescription()) && $entity->translate($content["lang"])->getDescription() != $content["description"]) || empty($entity->translate($content["lang"])->getDescription())) {
-                       $entity->translate($content["lang"])->setDescription() = $content["description"];
-                       $entity->mergeNewTranslations();
-                } 
-
-                $content = "update dynasty";              
-            } else {
-                $entity = new $entityClass();
-                $entity->setName($content["name"]);
+        if ($entity) {
+            if($entity->getBirth() != $content["birth"]) {
                 $entity->setBirth($content["birth"]);
-                $entity->setDeath($content["death"]);
-                $entity->translate($content["lang"])->setDescription($content["description"]);
-                $entity->mergeNewTranslations();
-                $content = "new ".$content["entity"]." created.";
-            }       
-            
-            $entityManager->persist($entity);
-            $entityManager->flush();
-        
-            } else {
-                $content = "the ".$entity." entity doesn't exist.";
             }
-        } else {
-            $content = "le json est invalide";
-        }
-        $data = $this->get('jms_serializer')->serialize($content, 'json');
+            if($entity->getDeath($content["death"])!=  $content["death"] ) {
+                $entity->setDeath($content["death"]);
+            }
+            if((!empty($entity->translate($content["lang"])->getDescription()) && $entity->translate($content["lang"])->getDescription() != $content["description"]) || empty($entity->translate($content["lang"])->getDescription())) {
+               $entity->translate($content["lang"])->setDescription($content["description"]);
+               $entity->mergeNewTranslations();
+           } 
 
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
-    }
+           $content = "update dynasty";              
+       } else {
+        $entity = new $entityClass();
+        $entity->setName($content["name"]);
+        $entity->setBirth($content["birth"]);
+        $entity->setDeath($content["death"]);
+        $entity->translate($content["lang"])->setDescription($content["description"]);
+        $entity->mergeNewTranslations();
+        $content = "new ".$content["entity"]." created.";
+    }       
+
+    $entityManager->persist($entity);
+    $entityManager->flush();
+
+} else {
+    $content = "le json est invalide";
+}
+$data = $this->get('jms_serializer')->serialize($content, 'json');
+
+$response = new Response($data);
+$response->headers->set('Content-Type', 'application/json');
+return $response;
+}
 
     /**
      * @Route("/api/add_discount", name="add_discount", methods={"POST"})
@@ -174,7 +184,7 @@ class ApiController extends Controller
         $content = json_decode($request->getContent(), true);
 
         if (isset($content["name"]) && isset($content["value"]) ) {
-            $entityClass = 'App\Entity\Discount');
+            $entityClass = 'App\Entity\Discount';
 
             $entityManager = $this->getDoctrine()->getManager();
             $entity = $entityManager->getRepository($entityClass)->findOneByName($content["name"]);
@@ -193,10 +203,7 @@ class ApiController extends Controller
             
             $entityManager->persist($entity);
             $entityManager->flush();
-        
-            } else {
-                $content = "the ".$entity." entity doesn't exist.";
-            }
+
         } else {
             $content = "le json est invalide";
         }
@@ -215,7 +222,7 @@ class ApiController extends Controller
         $content = json_decode($request->getContent(), true);
 
         if (isset($content["name"]))  {
-            $entityClass = 'App\Entity\SizeCategory');
+            $entityClass = 'App\Entity\SizeCategory';
 
             $entityManager = $this->getDoctrine()->getManager();
             $entity = $entityManager->getRepository($entityClass)->findOneByName($content["name"]);
@@ -225,7 +232,7 @@ class ApiController extends Controller
                 $entity->setName($content["name"]);
                 $entityManager->persist($entity);
                 $entityManager->flush();
-                }      
+            }      
         } else {
             $content = "le json est invalide";
         }
@@ -241,18 +248,19 @@ class ApiController extends Controller
      */
     public function getSizeCategoryAction( )
     {
-            $entityClass = 'App\Entity\SizeCategory');
+        $entityClass = 'App\Entity\SizeCategory';
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $sizeCategories = $entityManager->getRepository($entityClass)->findAll();
+        $entityManager = $this->getDoctrine()->getManager();
+        $sizeCategories = $entityManager->getRepository($entityClass)->findAll();
         $sizeCategoryList = array();
         if( !empty($sizeCategories)) {
             foreach($sizeCategories as $sizeCategory){
                 array_push($sizeCategoryList, $sizeCategory->getCategoryName());
             }
+        }
 
            //$data = $this->get('jms_serializer')->serialize($translations, 'json');
-           $data = json_encode($sizeCategoryList) ;
+        $data = json_encode($sizeCategoryList) ;
         $data = $this->get('jms_serializer')->serialize($content, 'json');
 
         $response = new Response($data);
@@ -261,51 +269,28 @@ class ApiController extends Controller
     }
 
     /**
-     * @Route("/api/getArtistsList", name="get_size_category", methods={"GET"})
+     * @Route("/api/getElementList/{element}", name="get_element_list", methods={"GET"})
      */
-    public function getSizeCategoryAction( )
+    public function getElementListAction( string $element )
     {
-            $entityClass = 'App\Entity\SizeCategory');
+        $entityClass = 'App\Entity\\'.ucfirst($element);
 
+        if (class_exists($entityClass)) {
             $entityManager = $this->getDoctrine()->getManager();
-            $sizeCategories = $entityManager->getRepository($entityClass)->findAll();
-        $sizeCategoryList = array();
-        if( !empty($sizeCategories)) {
-            foreach($sizeCategories as $sizeCategory){
-                array_push($sizeCategoryList, $sizeCategory->getCategoryName());
+
+            $elements = $entityManager->getRepository($entityClass)->findAll();
+
+            $elementList = array();
+            if( !empty($elements)) {
+                foreach($elements as $element){
+                    array_push($sizeCategoryList, $element->getName());
+                }
             }
 
            //$data = $this->get('jms_serializer')->serialize($translations, 'json');
-           $data = json_encode($sizeCategoryList) ;
-        $data = $this->get('jms_serializer')->serialize($content, 'json');
-
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
-    }
-
-    /**
-     * @Route("/api/getElementList/{element}", name="get_size_category", methods={"GET"})
-     */
-    public function getSizeCategoryAction( string $element )
-    {
-            $entityClass = 'App\Entity\\'.ucfirst($element));
-
- if (class_exists($entityClass)) {
-            $entityManager = $this->getDoctrine()->getManager();
-     
-        $elements = $entityManager->getRepository($entityClass)->findAll();
-
-        $elementList = array();
-        if( !empty($elements)) {
-            foreach($elements as $element){
-                array_push($sizeCategoryList, $element->getName());
-            }
-
-           //$data = $this->get('jms_serializer')->serialize($translations, 'json');
-           $data = json_encode($sizeCategoryList) ;
-        $data = $this->get('jms_serializer')->serialize($content, 'json');
-
+            $data = json_encode($sizeCategoryList) ;
+            $data = $this->get('jms_serializer')->serialize($content, 'json');
+        } else { $data = "enity ".$element." doesn't exist.";}
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
         return $response;
@@ -316,15 +301,15 @@ class ApiController extends Controller
      */
     public function getOneElementAction( string $entity, string $lang, string $name )
     {
-            $entityClass = 'App\Entity\\'.ucfirst($entity));
+        $entityClass = 'App\Entity\\'.ucfirst($entity);
 
         if (class_exists($entityClass)) {
             $entityManager = $this->getDoctrine()->getManager();
-     
-        $element = $entityManager->getRepository($entityClass)->findOneByName($name);
-        $elementDescriptionTranslation = $element->translate($lang)->getDescription();
 
-        $data = $this->get('jms_serializer')->serialize($element, 'json');
+            $element = $entityManager->getRepository($entityClass)->findOneByName($name);
+            $elementDescriptionTranslation = $element->translate($lang)->getDescription();
+
+            $data = $this->get('jms_serializer')->serialize($element, 'json');
         } else {
             $data = "this element doesn't exist";
         }
@@ -332,5 +317,41 @@ class ApiController extends Controller
         $response->headers->set('Content-Type', 'application/json');
         return $response;
     }
+
+    /**
+     * @Route("/api/translationlist/{entity}", name="get_translation_list", methods={"GET"})
+     */
+    public function getTranslationListAction( string $entity )
+    {
+        $entityClass = 'App\Entity\\'.ucfirst($entity);
+
+        if (class_exists($entityClass)) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $placeholders = $entityManager->getRepository($entityClass)->findAll();
+
+            $translationList = array();
+            if( !empty($placeholders)) {
+                $i =0;
+                foreach ($placeholders as $element){
+                   $translations = array();
+                   $translations["placeholder"] = $element->getPlaceholder();
+                   foreach ($this->languages as $lang) {
+                    $translations[$lang] = $element->translate($lang)->getName();
+                }
+
+                $translationList[$i] = $translations;
+                $i++;
+            }
+        }
+
+           //$data = $this->get('jms_serializer')->serialize($translations, 'json');
+          // $data = json_encode($translationList) ;
+        $data = $this->get('jms_serializer')->serialize($translationList, 'json');
+    } else { $data = "enity ".$element." doesn't exist.";}
+    $response = new Response($data);
+    $response->headers->set('Content-Type', 'application/json');
+    return $response;
+}
 
 }
